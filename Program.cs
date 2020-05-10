05e84678019670757b9bed08cb68e02c70dea54e
shadowue@hotmail.com <shadowue@hotmail.com>
2020-05-10 13:22:02 +0200

    Added conn creating to main

--- Program.cs
+++ Program.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using MeraClient2.Coms;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -10,6 +11,46 @@ namespace MeraClient2
     {
         static void Main(string[] args)
         {
+            Console.WriteLine("Client started...");
+            
+            Console.WriteLine("Selecting server:");
+            IComService server = new MeraServerComService();
+
+            Console.WriteLine("---------------------");
+
+            int inputSelection = SelectUserInput();
+            
+            Console.ReadKey();
+        }
+
+        static private int SelectUserInput()
+        {
+            string userinput = "user input";
+            string dbinput = "from db";
+            string fileinput = "from file";
+            Console.WriteLine($"Select method (type number 1, 2, 3):\n -1 {userinput} \n -2 {dbinput} \n -3 {fileinput}");
+            int x;
+          
+            do
+            {
+                Int32.TryParse(Console.ReadLine(), out x);
+            } while (x > 3 || x < 0);
+
+            switch (x)
+            {
+                case 1: 
+                    Console.WriteLine($"You selected input {userinput}");
+                    break;
+                case 2:
+                    Console.WriteLine($"You selected input {dbinput}");
+                    break;
+                case 3:
+                    Console.WriteLine($"You selected input {fileinput}");
+                    break;
+                default:
+                    break;
+            }
+            return x;
         }
     }
 }
