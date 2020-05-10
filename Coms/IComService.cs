3a4bc421bf1ad95225d1b7dc8a6c3b192be334b1
shadowue@hotmail.com <shadowue@hotmail.com>
2020-05-10 13:19:08 +0200

    Added ComServices

---
+++ Coms/IComService.cs
@@ -0,0 +1,16 @@
+﻿using DTOs;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MeraClient2.Coms
+{
+    public interface IComService
+    {
+        Task<int> GetWordsCount(string text);
+        Task<TextContainer> GetArticleBySubject(string subject);
+        Task<Catalog> GetArticlesList();
+    }
+}
